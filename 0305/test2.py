9f58c3bbddf50b000b4a7e917f8dc3387f4145e4
win3t0 <80086171+win3t0@users.noreply.github.com>
2021-03-05 15:57:37 +0900

    Add files via upload

---
+++ 0305/test2.py
@@ -0,0 +1,6 @@
+#학번 stdNum
+stdNum = input("학번 : ")
+#이름 stdName
+stdName = input("이름 : ")
+#둘다 출력
+print("당신의 학번은 ", stdNum, "이고 이름은 ", stdName, "입니다.")
---
+++ 0305/test3.py
@@ -0,0 +1,6 @@
+#학번
+stdNum = input("학번 : ")
+#이름
+stdName = input("이름 : ")
+#둘다 쓰세요
+print("당신의 학번은", stdNum,"이고, 이름은",stdName,"입니다.")
